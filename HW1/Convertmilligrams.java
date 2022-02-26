aad50d62de530b56e990e596d8ccc4850bc263d0
A-Abra <100457271+A-Abra@users.noreply.github.com>
2022-02-26 01:39:31 -0500

    Create Convertmilligrams.java

---
+++ HW1/Convertmilligrams.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Convertmilligrams {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter your weight in milligrams:");
+		int weight = s.nextInt();
+		int kilo = weight/1000000;
+		int gram = (weight-kilo*1000000)/1000;
+		int milli = (weight-kilo*1000000)-(gram*1000);
+		System.out.println("Your weight in kilos, grams, and milligrams: "+kilo+" kilos "+gram+" grams "+milli+" milligrans");
+	}
+}
