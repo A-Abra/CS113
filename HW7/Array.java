37129cb187ba9891a832ebd1b9a5157861c22e3b
A-Abra <100457271+A-Abra@users.noreply.github.com>
2022-02-27 23:35:28 -0500

    Add files via upload

--- HW7/Array.java
+++ HW7/Array.java
@@ -1,1 +1,22 @@
+package HWSeven;
 
+import java.util.*;
+
+public class Array {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String[][] info = new String[4][3];
+		for(int i = 0; i < info.length; i++) {
+			System.out.println("Enter first/last name and zipcode");
+			for(int j = 0; j < info[0].length; j++)
+				info[i][j] = s.nextLine();
+		}
+		
+		for (int i = 0; i < info.length; i++) {
+			System.out.println();
+			System.out.println("Input"+(i+1));
+			for (int j = 0; j < info[0].length; j++)
+				System.out.println(info[i][j]);
+		}
+	}
+}
